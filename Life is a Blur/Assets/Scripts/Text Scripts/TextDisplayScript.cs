52160397c4b706e5e9b7bfa688af536316e7fb0f
FSdelosreyes <fsdelosreyes@student.apc.edu.ph>
2022-04-20 13:58:37 +0800

    Update to Text Test Scene

--- Life is a Blur/Assets/Scripts/Text Scripts/TextDisplayScript.cs
+++ Life is a Blur/Assets/Scripts/Text Scripts/TextDisplayScript.cs
@@ -6,14 +6,26 @@ using TMPro;
 
 public class TextDisplayScript : MonoBehaviour
 {
+    [Header("Text")]
     [Range(0.0f, 1.0f)]
     public float displayDelay;
     public string fullText;
-    private string currentText = "";
+    string currentText = "";
+
+    [Header("TextBox")]
+    [Range(0.0f, 1.0f)]
+    public float currentLerp;
+    [Range(0.0f, 1.0f)]
+    public float lerpRate;
+    public bool isDone = true;
+    public bool isDisplayed = false;
+    public Image textBackground;
+
 
     public void DisplayText()
     {
         currentText = "";
+        this.GetComponent<TMP_Text>().color = Color.white;
         StartCoroutine(ShowText());
     }
 
@@ -24,6 +36,46 @@ public class TextDisplayScript : MonoBehaviour
             currentText = fullText.Substring(0, i);
             this.GetComponent<TMP_Text>().text = currentText;
             yield return new WaitForSeconds(displayDelay);
+        }
+    }
+
+    public void FadeInTextBG()
+    {
+        isDone = false;
+        isDisplayed = false;
+    }
+    public void FadeOutTextBG()
+    {
+        isDone = false;
+        isDisplayed = true;
+    }
+
+    public void Update()
+    {
+        //Lerping
+        if (!isDone)
+        {
+            Debug.Log("Lerp to black");
+            textBackground.color = Color.Lerp(Color.clear, Color.black, currentLerp);
+            if (!isDisplayed)
+            {
+                currentLerp += lerpRate * Time.deltaTime;
+            }
+            if (isDisplayed)
+            {
+                currentLerp -= lerpRate * Time.deltaTime;
+                this.GetComponent<TMP_Text>().color = Color.Lerp(Color.clear, Color.white, currentLerp);
+            }
+            if (currentLerp <= 0)
+            {
+                isDone = true;
+                currentLerp = 0;
+            }
+            if (currentLerp >= 1)
+            {
+                isDone = true;
+                currentLerp = 1;
+            }
         }
     }
 }
