f62d50c0a3898f4d599695e67c7fbf25f002ee0a
FSdelosreyes <fsdelosreyes@student.apc.edu.ph>
2022-11-04 12:56:52 +0800

    Testing on script to add visual feedback on menu buttons

---
+++ Life is a Blur/Assets/TempScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TempScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public TextMeshProUGUI baseText;
+
+    //Testing
+    public Color baseColor;
+    public Color targetColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        baseText = this.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        baseColor = baseText.color;
+
+        targetColor = Color.yellow;
+    }
+
+    //Do Things On Hover
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Hovering");
+        baseText.color = targetColor;
+    }
+
+    //Do Things On Exit (Revert What On Enter Does)
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Not Hovering");
+        baseText.color = baseColor;
+    }
+}
