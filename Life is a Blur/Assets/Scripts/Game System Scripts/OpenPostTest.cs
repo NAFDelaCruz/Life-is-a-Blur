8440c5ab08210ffc0e60bb3cf4119aacdd670e71
Niel Dela Cruz <nielangelodelacruz@gmail.com>
2022-11-05 21:41:31 +0800

    Small Bug Fix
    small fix was done found at the end because of double canvas.

--- Life is a Blur/Assets/Scripts/Game System Scripts/OpenPostTest.cs
+++ Life is a Blur/Assets/Scripts/Game System Scripts/OpenPostTest.cs
@@ -8,6 +8,7 @@ public class OpenPostTest : MonoBehaviour
     public void OpenLink()
     {
         Application.OpenURL("https://forms.gle/RrwjG2e6eRcX2h9R6");
+        PlayerPrefs.SetInt("Current Level", SceneManager.GetActiveScene().buildIndex);
         SceneManager.LoadScene(0);
     }
 }
